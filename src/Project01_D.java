84e0fc3bf64c66d9a1dab99959eb06159ea9308d
jjimini98 <jjimini98@naver.com>
2021-07-18 01:40:57 +0900

    JavaAPI 7강 완료

--- src/Project01_D.java
+++ src/Project01_D.java
@@ -1,3 +1,7 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -24,11 +28,35 @@ public class Project01_D {
             conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID",client_id);
             conn.setRequestProperty("X-NCP-APIGW-API-KEY",client_secret);
 
+            BufferedReader br ;
             int responsecode = conn.getResponseCode();
-            if (responsecode == 200) {}
+            if (responsecode == 200) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); }
+            else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
 
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = br.readLine())!=null) {
+                response.append(line);
+            }
+            br.close();
 
+            JSONTokener tokener = new JSONTokener(response.toString());
+            JSONObject object = new JSONObject(tokener);
 
+            System.out.println(object.toString(2));
+
+            JSONArray arr = object.getJSONArray("addresses");
+
+            for(int i =0 ; i< arr.length();i++) {
+                JSONObject temp  = (JSONObject)  arr.get(i);
+                System.out.println("address :" + temp.get("roadAddress"));
+                System.out.println("jibunaddress :" + temp.get("jibunAddress"));
+                System.out.println("위도 :" + temp.get("x"));
+                System.out.println("경도 :" + temp.get("y"));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
